141ae4fda20dd84148ec251a205fb5ae9860c936
Zack01234 <limonod57@gmail.com>
2022-09-01 12:33:14 +0500

    первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Задание 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.printf("ФИО сотрудника — %s.", fullName);
+        System.out.println();
+
+        //Задание 2
+        String fullName1 = fullName.toUpperCase();
+        System.out.printf("Данные ФИО сотрудника для заполнения отчета — %s.", fullName1);
+        System.out.println();
+
+        //Задание 3
+        String firstName2 = "Семён";
+        String middleName2 = "Семёнович";
+        String lastName2 = "Иванов";
+        String fullName2 = lastName2 + " " + firstName2 + " " + middleName2;
+        System.out.printf("Данные ФИО сотрудника — %s.", fullName2.replace('ё', 'е'));
+        System.out.println();
+
     }
 }
